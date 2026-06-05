c457747cd80ab2543558c098c25a111a29706ece
sprainedtootsie <105941968+sprainedtootsie@users.noreply.github.com>
2026-06-05 14:32:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-fetch('header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header-placeholder').innerHTML = data;
-  });
-
 const menuBtn = document.getElementById('menuBtn');
 const navMenu = document.getElementById('navMenu');
 
