09797f31e3e94763f397be98680b1fc181f70d39
sprainedtootsie <105941968+sprainedtootsie@users.noreply.github.com>
2026-06-05 08:43:05 -0500

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo isset($pageTitle) ? $pageTitle : "My Website"; ?></title>
-    <link rel="stylesheet" href="style.css">
+        <meta charset="UTF-8">
+        <title>Art by Liberty Jensen</title>
+
+        <!--Questrial font from Google for Header text-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Questrial&family=Titan+One&display=swap" rel="stylesheet">
+
+        <!--Roboto font from Google for general text-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Fjalla+One&family=Questrial&family=Roboto:ital,wght@0,100..900;1,100..900&family=Titan+One&display=swap" rel="stylesheet">
+    
+        <link rel="stylesheet" href="stylesheet"/>
 </head>
 <body>
     <header>
         <nav>
             <a href="index.php">Home</a> | 
-            <a href="about.php">About</a> | 
+            <a href="portfolio.php">Portfolio</a> | 
             <a href="contact.php">Contact</a>
         </nav>
     </header>
