f53a9189b3457e9fa79c3bbb74f802da73733095
sprainedtootsie <105941968+sprainedtootsie@users.noreply.github.com>
2026-06-05 15:22:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
   navMenu.classList.toggle('open');
 });
+
+fetch('navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading HTML:', error));
