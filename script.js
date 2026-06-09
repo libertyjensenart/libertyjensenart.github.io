fb253bf1a84babca2c749fe4500a13ce6c825e98
sprainedtootsie <105941968+sprainedtootsie@users.noreply.github.com>
2026-06-09 09:35:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
-// Header Menu Functions
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('header-placeholder').innerHTML = data;
+    
+    // Header Menu Functions
 const menuBtn = document.getElementById('menuBtn');
 const navMenu = document.getElementById('navMenu');
 
@@ -7,10 +11,6 @@ menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('open');
   navMenu.classList.toggle('open');
 });
-
-fetch('header.html')
-    .then(response => response.text())
-    .then(data => {
-      document.getElementById('header-placeholder').innerHTML = data;
+    
     })
     .catch(error => console.error('Error loading HTML:', error));
